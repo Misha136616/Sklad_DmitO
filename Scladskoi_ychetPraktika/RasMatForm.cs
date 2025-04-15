d0fd46e9b511c1a247926a4cb0a5d9c9bf9896c8
kol4egraf0-0 <genitya408@gmail.com>
2025-04-15 14:38:32 +0300

    fast supl form

--- Scladskoi_ychetPraktika/RasMatForm.cs
+++ Scladskoi_ychetPraktika/RasMatForm.cs
@@ -31,5 +31,204 @@ namespace Scladskoi_ychetPraktika
             this.suppliesTableAdapter.Fill(this.praktik_PDataSet.supplies);
 
         }
+        /*
+                 private bool validateClient()
+        {
+            // Проверяем, что все текстовые поля не пустые
+            if (!string.IsNullOrWhiteSpace(nameTextBox.Text) ||
+                !string.IsNullOrWhiteSpace(contact_infoTextBox.Text) ||
+                !string.IsNullOrWhiteSpace(addressTextBox.Text) ||
+                !string.IsNullOrWhiteSpace(client_typeTextBox.Text))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void KlientsForm_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "prdoDataSet.clients". При необходимости она может быть перемещена или удалена.
+            this.clientsTableAdapter.Fill(this.prdoDataSet.clients);
+            
+        }
+
+        
+
+        private void btnDobav_Click(object sender, EventArgs e)
+        {
+            if (validateClient()) 
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    
+                    string query = @"
+            INSERT INTO clients (name, contact_info, address, client_type)
+            VALUES (@Name, @ContactInfo, @Address, @ClientType);";
+
+                    using (SqlCommand sqlCommand = new SqlCommand(query, connection))
+                    {
+                        try
+                        {
+                            connection.Open();
+                            sqlCommand.CommandType = CommandType.Text;
+
+                            // Добавление параметров
+                            sqlCommand.Parameters.AddWithValue("@Name", nameTextBox.Text); 
+                            sqlCommand.Parameters.AddWithValue("@ContactInfo", contact_infoTextBox.Text); 
+                            sqlCommand.Parameters.AddWithValue("@Address", addressTextBox.Text);
+                            if (client_typeTextBox.Text != null)
+                            {
+                                sqlCommand.Parameters.AddWithValue("@ClientType", client_typeTextBox.Text.ToString());
+                            }
+                            else
+                            {
+                                MessageBox.Show("Выберите тип клиента из списка");
+                                return; 
+                            }
+
+                            // Выполнение запроса
+                            sqlCommand.ExecuteNonQuery();
+                            MessageBox.Show("Клиент успешно добавлен");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Ошибка добавления клиента: " + ex.Message);
+                        }
+                        finally
+                        {
+                            connection.Close();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Незаполненные поля или неверный формат данных");
+            }
+        }
+
+        private void btnYdal_Click(object sender, EventArgs e)
+        {
+            if (clientsDataGridView.CurrentRow != null)
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "DELETE FROM clients WHERE id = @Id";
+                    using (SqlCommand sqlCommand = new SqlCommand(query, connection))
+                    {
+                        try
+                        {
+                            connection.Open();
+                            sqlCommand.CommandType = CommandType.Text;
+                            sqlCommand.Parameters.AddWithValue("@Id", clientsDataGridView.CurrentRow.Cells["id"].Value);
+                            sqlCommand.ExecuteNonQuery();
+                            MessageBox.Show("Клиент успешно удален");
+                            clientsBindingSource.RemoveCurrent();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Ошибка удаления клиента: " + ex.Message);
+                        }
+                        finally
+                        {
+                            connection.Close();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnObnov_Click(object sender, EventArgs e)
+        {
+            if (validateClient())
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = @"
+UPDATE clients 
+SET name = @Name, contact_info = @ContactInfo, address = @Address, client_type = @ClientType
+WHERE id = @Id";
+
+                    using (SqlCommand sqlCommand = new SqlCommand(query, connection))
+                    {
+                        try
+                        {
+                            connection.Open();
+                            sqlCommand.CommandType = CommandType.Text;
+
+                            sqlCommand.Parameters.AddWithValue("@Id", clientsDataGridView.CurrentRow.Cells["id"].Value);
+                            sqlCommand.Parameters.AddWithValue("@Name", nameTextBox.Text);
+                            sqlCommand.Parameters.AddWithValue("@ContactInfo", contact_infoTextBox.Text);
+                            sqlCommand.Parameters.AddWithValue("@Address", addressTextBox.Text);
+                            sqlCommand.Parameters.AddWithValue("@ClientType", client_typeTextBox.Text.ToString());
+
+                            sqlCommand.ExecuteNonQuery();
+                            MessageBox.Show("Данные клиента успешно обновлены");
+                            clientsBindingSource.EndEdit();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Ошибка обновления клиента: " + ex.Message);
+                        }
+                        finally
+                        {
+                            connection.Close();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Незаполненные поля или неверный формат данных");
+            }
+        }
+
+        private void txtProductSearch_TextChanged(object sender, EventArgs e)
+        {
+            clientsBindingSource.Filter = string.Format("name LIKE '%{0}%'", txtProductSearch.Text);
+        }
+
+        private void butSortir_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                clientsBindingSource.Sort = "name ASC";
+            }
+            else if (radioButton2.Checked)
+            {
+                clientsBindingSource.Sort = "name DESC";
+            }
+        }
+
+        private void btnOchist_Click(object sender, EventArgs e)
+        {
+            txtProductSearch.Clear();
+        }
+        private void LoadProducts()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM clients";
+
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                {
+                    try
+                    {
+                        DataTable table = new DataTable();
+                        adapter.Fill(table);
+                        clientsDataGridView.DataSource = table;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ошибка загрузки данных: " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+         */
     }
 }
